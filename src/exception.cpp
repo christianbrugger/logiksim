024ba9635390a9341c44b5b34f5b986f961a5e8c
Christian Brugger <brugger.chr@gmail.com>
2023-11-03 15:38:37 +0800

    removed boost-stracktrace && fixed clang debug test compilation

--- src/exception.cpp
+++ src/exception.cpp
@@ -1,6 +1,5 @@
 #include "exception.h"
 
-#include <boost/stacktrace.hpp>
 #include <fmt/core.h>
 
 #include <stdexcept>
@@ -9,18 +8,8 @@ namespace logicsim {
 
 namespace detail {
 
-constexpr static inline auto enable_exception_traceback = false;
-
 [[noreturn]] auto throw_exception_impl(const char *msg) -> void {
-    const auto full_msg = [&] {
-        if constexpr (enable_exception_traceback) {
-            const auto tr = boost::stacktrace::to_string(boost::stacktrace::stacktrace());
-            return fmt::format("{}\nException: {}\n", tr, msg);
-        } else {
-            return fmt::format("\nException: {}\n", msg);
-        }
-    }();
-
+    const auto full_msg = fmt::format("\nException: {}\n", msg);
     throw std::runtime_error(full_msg.c_str());
 }
 
