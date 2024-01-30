a05fc5c1eca142db243e83e42bbb73decfa36cfa
Christian Brugger <brugger.chr@gmail.com>
2024-01-30 17:23:49 +0800

    improved exception safety & robustness of setting dialogs manager

--- src/setting_dialog_manager.cpp
+++ src/setting_dialog_manager.cpp
@@ -19,6 +19,21 @@ SettingDialogManager::SettingDialogManager(QWidget* parent)
 
 namespace {
 
+auto get_selected_logic_item(const EditableCircuit& editable_circuit,
+                             selection_id_t selection_id) -> logicitem_id_t {
+    if (!editable_circuit.selection_exists(selection_id)) {
+        return null_logicitem_id;
+    }
+    const auto& selection = editable_circuit.selection(selection_id);
+
+    if (selection.selected_logic_items().size() != 1 ||
+        selection.selected_segments().size() != 0) {
+        return null_logicitem_id;
+    }
+
+    return selection.selected_logic_items().front();
+}
+
 auto create_setting_dialog(const EditableCircuit& editable_circuit,
                            setting_handle_t setting_handle, QWidget* parent,
                            selection_id_t selection_id) -> SettingDialog* {
@@ -41,33 +56,39 @@ auto SettingDialogManager::show_setting_dialog(EditableCircuit& editable_circuit
                                                setting_handle_t setting_handle) -> void {
     // find existing dialog
     for (auto&& [selection_id, widget] : map_) {
-        if (editable_circuit.selection_exists(selection_id) &&
-            editable_circuit.selection(selection_id)
-                .is_selected(setting_handle.logicitem_id)) {
+        if (get_selected_logic_item(editable_circuit, selection_id) ==
+            setting_handle.logicitem_id) {
             widget->show();
             widget->activateWindow();
             return;
         }
     }
 
-    // create new dialog
-    // TODO EXCEPTION SAFETY  !!!
+    // created tracked selection
     const auto selection_id = editable_circuit.create_selection();
-    editable_circuit.selection(selection_id).add(setting_handle.logicitem_id);
+    Expects(selection_id);
 
+    try {
+        editable_circuit.selection(selection_id).add(setting_handle.logicitem_id);
+        const auto [it, inserted] = map_.emplace(selection_id, nullptr);
+        Expects(inserted);
+
+    } catch (std::exception&) {
+        editable_circuit.destroy_selection(selection_id);
+        throw;
+    }
+
+    // create dialog
     auto* widget =
         create_setting_dialog(editable_circuit, setting_handle, parent_, selection_id);
     Expects(widget);
 
-    // add to map
     try {
         connect(widget, &QWidget::destroyed, this,
                 &SettingDialogManager::on_dialog_destroyed);
         connect(widget, &SettingDialog::attributes_changed, this,
                 &SettingDialogManager::on_attributes_changed);
-
-        const auto [it, inserted] = map_.emplace(selection_id, widget);
-        Expects(inserted);
+        map_.at(selection_id) = widget;
     } catch (std::exception&) {
         widget->deleteLater();
         throw;
@@ -87,14 +108,12 @@ auto SettingDialogManager::close_all(EditableCircuit& editable_circuit) -> void 
 }
 
 auto SettingDialogManager::run_cleanup(EditableCircuit& editable_circuit) -> void {
-    print("TODO EXCEPTION SAFETY");
     print("TODO close all dialogs & finalize editing when loading new circuit");
 
-    // close dialogs with empty selections
+    // close dialogs with deleted logic-items
     for (auto&& [selection_id, widget] : map_) {
         if (widget) {
-            if (!editable_circuit.selection_exists(selection_id) ||
-                editable_circuit.selection(selection_id).empty()) {
+            if (!get_selected_logic_item(editable_circuit, selection_id)) {
                 widget->deleteLater();
                 widget = nullptr;
             }
@@ -110,7 +129,7 @@ auto SettingDialogManager::run_cleanup(EditableCircuit& editable_circuit) -> voi
     }
 
     // free selections
-    for (auto selection_id : delete_list) {
+    for (const auto& selection_id : delete_list) {
         save_destroy_selection(editable_circuit, selection_id);
         Expects(map_.erase(selection_id) == 1);
     }
@@ -136,30 +155,6 @@ Q_SLOT void SettingDialogManager::on_attributes_changed(selection_id_t selection
 // Public Methods
 //
 
-namespace {
-
-auto get_selected_logic_item(const EditableCircuit& editable_circuit,
-                             selection_id_t selection_id) -> logicitem_id_t {
-    if (!editable_circuit.selection_exists(selection_id)) {
-        return null_logicitem_id;
-    }
-
-    const auto& selection = editable_circuit.selection(selection_id);
-
-    if (selection.selected_logic_items().size() > 1 ||
-        selection.selected_segments().size() > 0) {
-        throw std::runtime_error(
-            "unexpected selected items size in SettingWidgetRegistry");
-    }
-    if (selection.selected_logic_items().size() == 0) {
-        return null_logicitem_id;
-    }
-
-    return selection.selected_logic_items().front();
-}
-
-}  // namespace
-
 auto change_setting_attributes(EditableCircuit& editable_circuit,
                                selection_id_t selection_id, SettingAttributes attributes)
     -> void {
@@ -169,7 +164,10 @@ auto change_setting_attributes(EditableCircuit& editable_circuit,
         return;
     }
 
-    if (attributes.attrs_clock_generator) {
+    const auto logicitem_type = editable_circuit.layout().logic_items().type(element_id);
+
+    if (logicitem_type == LogicItemType::clock_generator &&
+        attributes.attrs_clock_generator) {
         editable_circuit.set_attributes(element_id,
                                         attributes.attrs_clock_generator.value());
     }
