b8e98274fe1e22a063b9a413787ecac5dceb2415
Christian Brugger <brugger.chr@gmail.com>
2024-08-18 17:37:05 +0700

    moved dependencies to core library cmake file

--- cmake/Modules/FindZLIB.cmake
+++ cmake/Modules/FindZLIB.cmake
@@ -3,7 +3,12 @@
 if (TARGET ZLIB::ZLIB)
     set(ZLIB_FOUND TRUE)
 else()
-    add_subdirectory(external/zlib EXCLUDE_FROM_ALL SYSTEM)
+    add_subdirectory(
+        ${PROJECT_SOURCE_DIR}/external/zlib
+        ${CMAKE_BINARY_DIR}/external/zlib
+        EXCLUDE_FROM_ALL
+        SYSTEM
+    )
 
     add_library(ZLIB::ZLIB ALIAS zlibstatic)
     set(ZLIB_FOUND TRUE)
