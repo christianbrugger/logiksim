f47ad74ee3902e7e43c950084e9893b4d691d7a6
Christian Brugger <brugger.chr@gmail.com>
2024-08-19 22:52:39 +0700

    moved project options to main file

--- cmake/project_options.cmake
+++ cmake/project_options.cmake
@@ -1,39 +1,39 @@
 
 
-function(ls_define_program_options)
-    option(LS_ENABLE_TIME_TRACE "Trace compilation times. Only works for Clang." OFF)
-    option(LS_ENABLE_CCACHE "Enable ccache." OFF)
-    option(LS_ENABLE_LTO "Enable link time optimization." OFF)
-    option(LS_ENABLE_PCH "Enable pre-compiled headers." ON)
-    option(LS_ENABLE_COVERAGE "Enable coverage for tests under Clang" OFF)
+# function(ls_define_project_options)
+#     option(LS_ENABLE_TIME_TRACE "Trace compilation times. Only works for Clang." OFF)
+#     option(LS_ENABLE_CCACHE "Enable ccache." OFF)
+#     option(LS_ENABLE_LTO "Enable link time optimization." OFF)
+#     option(LS_ENABLE_PCH "Enable pre-compiled headers." ON)
+#     option(LS_ENABLE_COVERAGE "Enable coverage for tests under Clang" OFF)
+#
+#     # sanitizers
+#     set(LS_SANITIZE "" CACHE STRING
+#         "Enable sanitizers (Options: Address Undefined Address;Undefined 
+#         Memory MemoryWithOrigins Thread). Defaults to empty string.")
+#     set_property(
+#         CACHE LS_SANITIZE PROPERTY STRINGS 
+#         "" Address Undefined Address;Undefined Memory Thread
+#     )
+#
+#     # libc++
+#     set(LS_USE_LIBCXX AUTO CACHE STRING "Compile all dependencies against clang libc++.")
+#     set_property(CACHE LS_USE_LIBCXX PROPERTY STRINGS AUTO ON OFF)
+#
+#     if ((LS_USE_LIBCXX STREQUAL "AUTO" AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
+#            (LS_SANITIZE STREQUAL "Memory" OR  LS_SANITIZE STREQUAL "MemoryWithOrigins" OR 
+#             LS_SANITIZE STREQUAL "Thread")) 
+#         OR (NOT LS_USE_LIBCXX STREQUAL "AUTO" AND LS_USE_LIBCXX)
+#     )
+#         set(LS_USE_LIBCXX_BOOL_result TRUE)
+#     else()
+#         set(LS_USE_LIBCXX_BOOL_result FALSE)
+#     endif()
+#
+#     set(LS_USE_LIBCXX_BOOL ${LS_USE_LIBCXX_BOOL_result} CACHE INTERNAL "")
+# endfunction()
 
-    # sanitizers
-    set(LS_SANITIZE "" CACHE STRING
-        "Enable sanitizers (Options: Address Undefined Address;Undefined 
-        Memory MemoryWithOrigins Thread). Defaults to empty string.")
-    set_property(
-        CACHE LS_SANITIZE PROPERTY STRINGS 
-        "" Address Undefined Address;Undefined Memory Thread
-    )
-
-    # libc++
-    set(LS_USE_LIBCXX AUTO CACHE STRING "Compile all dependencies against clang libc++.")
-    set_property(CACHE LS_USE_LIBCXX PROPERTY STRINGS AUTO ON OFF)
-
-    if ((LS_USE_LIBCXX STREQUAL "AUTO" AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
-           (LS_SANITIZE STREQUAL "Memory" OR  LS_SANITIZE STREQUAL "MemoryWithOrigins" OR 
-            LS_SANITIZE STREQUAL "Thread")) 
-        OR (NOT LS_USE_LIBCXX STREQUAL "AUTO" AND LS_USE_LIBCXX)
-    )
-        set(LS_USE_LIBCXX_BOOL_result TRUE)
-    else()
-        set(LS_USE_LIBCXX_BOOL_result FALSE)
-    endif()
-
-    set(LS_USE_LIBCXX_BOOL ${LS_USE_LIBCXX_BOOL_result} CACHE INTERNAL "")
-endfunction()
-
-function(ls_print_program_options)
+function(ls_print_project_options)
     include(CMakePrintHelpers)
 
     cmake_print_variables(LS_ENABLE_TIME_TRACE)
