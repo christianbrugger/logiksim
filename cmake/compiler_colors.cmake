a58be90f7924f0e7df77dee07acd88d1c1356c7a
Christian Brugger <brugger.chr@gmail.com>
2024-08-22 16:21:35 +0700

    fixed color-flags for msvc & g++

--- cmake/compiler_colors.cmake
+++ cmake/compiler_colors.cmake
@@ -3,13 +3,13 @@
 #
 function(ls_enable_compiler_colors target_name)
     if (MSVC)
-        target_compile_options("${target_name}" INTERFACE "/diagnostics:column")
+        target_compile_options("${target_name}" INTERFACE "/diagnostics:caret")
 
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         target_compile_options("${target_name}" INTERFACE "-fcolor-diagnostics")
 
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        target_compile_options("${target_name}" INTERFACE "-fcolor-diagnostics=always")
+        target_compile_options("${target_name}" INTERFACE "-fdiagnostics-color=always")
     endif()
 
 endfunction()
