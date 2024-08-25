1b908b03acd32ac90dd1aa9b9fca6bb20a8da3b9
Christian Brugger <brugger.chr@gmail.com>
2024-08-25 22:03:51 +0800

    using new cmake 3.25 msvc debug format variable to fix ccache

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -2,6 +2,11 @@
 #
 # Setup ccache globally
 #
+# Ideally the properties set here would be set on the global interface target.
+# However, while the properties exist on the target, they are not inherited.
+# This macro sets them globally for all targets from here on.
+#
+
 macro(ls_setup_ccache do_enable)
     include(cmake/utils.cmake)
     ls_require_bool_value("${do_enable}")
@@ -11,23 +16,20 @@ macro(ls_setup_ccache do_enable)
     if ("${do_enable}" AND LS_CCACHE_PROGRAM)
         message(NOTICE "LOGIKSIM: Enabling ccache with executable '${LS_CCACHE_PROGRAM}'.")
 
-        set(CMAKE_CXX_COMPILER_LAUNCHER "${LS_CCACHE_PROGRAM}")
+        set(CMAKE_CXX_COMPILER_LAUNCHER 
+            "${LS_CCACHE_PROGRAM}"
+            # increase sloppiness so it works with pre-compiled headers
+            "sloppiness=pch_defines,time_macros,include_file_mtime,include_file_ctime"
+            # otherwise pre-processor is run twice on cache misses, increasing build time
+            # see https://github.com/ccache/ccache/discussions/1420#discussioncomment-8906839
+            "depend_mode=true"
+        )
 
-        # https://ccache.dev/manual/latest.html
-        set(ENV{CCACHE_NOHASHDIR} "true")
-        set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros,include_file_mtime,include_file_ctime")
-        set(ENV{CCACHE_DEPEND} "true")
-        # if (MSVC)
-        #     # TODO review, do we still need this?
-        #     set(ENV{CCACHE_COMPILERTYPE} "msvc")
-        # endif()
-
-        # TODO; review, can we do this another way
-        # ccache only supports /Z7, not /Zi
-        # string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
-        # string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
-        # string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
-        # string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+        # CMake default to ProgramDatabase (-Zi) debug information format.
+        # This is incompatible with ccache, as it producese a central database.
+        # CCache only supports the Embedded (-Z7) format.
+        set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT  
+            "$<$<CONFIG:Debug,RelWithDebInfo>:Embedded>")
 
     elseif("${do_enable}")
         message(WARNING "LOGIKSIM: Warning, requested ccache, but not found.")
--- cmake/standard_library.cmake
+++ cmake/standard_library.cmake
@@ -1,4 +1,43 @@
 
+function(ls_setup_standard_library_static target_name use_libcxx sanitizers)
+    include(cmake/utils.cmake)
+    ls_require_bool(use_libcxx)
+
+    if (use_libcxx)
+        message(NOTICE "LOGIKSIM: Using static clang libc++.")
+
+        set(LLVM_ENABLE_RUNTIMES "libcxx;libcxxabi;libunwind" CACHE STRING "" FORCE)
+
+        set(LIBCXX_ENABLE_SHARED OFF)
+        set(LIBCXXABI_ENABLE_SHARED OFF)
+        set(LIBCXX_ENABLE_STATIC ON)
+        set(LIBCXXABI_ENABLE_STATIC ON)
+
+        set(LLVM_USE_SANITIZER "${sanitizers}")
+        add_subdirectory(external/llvm-project/runtimes EXCLUDE_FROM_ALL SYSTEM)
+        target_link_libraries(
+            "${target_name}"
+            INTERFACE
+            cxx_static
+            cxxabi_static
+        )
+
+        # The google benchmark contains an unconditional export that requires
+        # all its dependencies to be exportable. By itself llvm does not define
+        # export for runtime targets. Thats why we define them here.
+        export(TARGETS 
+            cxx_static cxxabi_static cxx-headers cxxabi_static_objects
+            libcxx-abi-headers cxxabi-headers 
+            FILE "${CMAKE_CURRENT_BINARY_DIR}/cmake/LlvmRuntimeTargets.cmake"
+        )
+
+    else()
+        message(NOTICE "LOGIKSIM: Using default stdlib.")
+    endif()
+
+endfunction()
+
+
 function(ls_setup_standard_library_shared target_name use_libcxx sanitizers)
     include(cmake/utils.cmake)
     ls_require_bool(use_libcxx)
@@ -37,40 +76,3 @@ function(ls_setup_standard_library_shared target_name use_libcxx sanitizers)
 
 endfunction()
 
-function(ls_setup_standard_library_static target_name use_libcxx sanitizers)
-    include(cmake/utils.cmake)
-    ls_require_bool(use_libcxx)
-
-    if (use_libcxx)
-        message(NOTICE "LOGIKSIM: Using static clang libc++.")
-
-        set(LLVM_ENABLE_RUNTIMES "libcxx;libcxxabi;libunwind" CACHE STRING "" FORCE)
-
-        set(LIBCXX_ENABLE_SHARED OFF)
-        set(LIBCXXABI_ENABLE_SHARED OFF)
-        set(LIBCXX_ENABLE_STATIC ON)
-        set(LIBCXXABI_ENABLE_STATIC ON)
-
-        set(LLVM_USE_SANITIZER "${sanitizers}")
-        add_subdirectory(external/llvm-project/runtimes EXCLUDE_FROM_ALL SYSTEM)
-        target_link_libraries(
-            "${target_name}"
-            INTERFACE
-            cxx_static
-            cxxabi_static
-        )
-
-        # The google benchmark contains an unconditional export that requires
-        # all its dependencies to be exportable. By itself llvm does not define
-        # export for runtime targets. Thats why we define them here.
-        export(TARGETS 
-            cxx_static cxxabi_static cxx-headers cxxabi_static_objects
-            libcxx-abi-headers cxxabi-headers 
-            FILE "${CMAKE_CURRENT_BINARY_DIR}/cmake/LlvmRuntimeTargets.cmake"
-        )
-
-    else()
-        message(NOTICE "LOGIKSIM: Using default stdlib.")
-    endif()
-
-endfunction()
