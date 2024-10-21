17303d7c5e6f161bff2f9e0e4998a987a3efae02
Christian Brugger <brugger.chr@gmail.com>
2024-10-21 20:07:54 +0800

    Text-element crash for negative positions

--- src/core/algorithm/range_extended.h
+++ src/core/algorithm/range_extended.h
@@ -9,7 +9,6 @@
 #include <limits>
 #include <ranges>
 #include <type_traits>
-#include <utility>  // std::size_t
 
 namespace logicsim {
 
@@ -20,15 +19,15 @@ namespace logicsim {
  *      * value_type & T can hold all number [start, .. end)
  */
 template <typename T, typename value_type = typename T::value_type>
-    requires explicitly_convertible_to<std::size_t, value_type> &&
+    requires explicitly_convertible_to<std::ptrdiff_t, value_type> &&
              std::is_constructible_v<T, value_type>
-constexpr auto range_extended(std::size_t start, std::size_t end) {
-    Expects(end == 0 ||
-            end - 1 <= gsl::narrow<std::size_t>(std::numeric_limits<value_type>::max()));
+constexpr auto range_extended(std::ptrdiff_t start, std::ptrdiff_t end) {
+    Expects(end == 0 || end - 1 <= gsl::narrow<std::ptrdiff_t>(
+                                       std::numeric_limits<value_type>::max()));
 
     return std::ranges::views::transform(
         std::ranges::views::iota(start, end),
-        [](const std::size_t &v) -> T { return T {static_cast<value_type>(v)}; });
+        [](const std::ptrdiff_t &v) -> T { return T {static_cast<value_type>(v)}; });
 }
 
 /**
@@ -38,15 +37,15 @@ constexpr auto range_extended(std::size_t start, std::size_t end) {
  *      * value_type & T can hold all number [first, .. last]
  */
 template <typename T, typename value_type = typename T::value_type>
-    requires explicitly_convertible_to<std::size_t, value_type> &&
+    requires explicitly_convertible_to<std::ptrdiff_t, value_type> &&
              std::is_constructible_v<T, value_type>
-constexpr auto range_inclusive(std::size_t first, std::size_t last) {
-    Expects(last <= gsl::narrow<std::size_t>(std::numeric_limits<value_type>::max()));
-    Expects(last < std::numeric_limits<std::size_t>::max());
+constexpr auto range_inclusive(std::ptrdiff_t first, std::ptrdiff_t last) {
+    Expects(last <= gsl::narrow<std::ptrdiff_t>(std::numeric_limits<value_type>::max()));
+    Expects(last < std::numeric_limits<std::ptrdiff_t>::max());
 
     return std::ranges::views::transform(
-        std::ranges::views::iota(first, last + std::size_t {1}),
-        [](const std::size_t &v) -> T { return T {static_cast<value_type>(v)}; });
+        std::ranges::views::iota(first, last + std::ptrdiff_t {1}),
+        [](const std::ptrdiff_t &v) -> T { return T {static_cast<value_type>(v)}; });
 }
 
 /**
@@ -56,17 +55,17 @@ constexpr auto range_inclusive(std::size_t first, std::size_t last) {
  *      * value_type & T can hold all number [0, .. count)
  */
 template <typename T, typename value_type = typename T::value_type>
-    requires explicitly_convertible_to<std::size_t, value_type> &&
+    requires explicitly_convertible_to<std::ptrdiff_t, value_type> &&
              std::is_constructible_v<T, value_type>
-constexpr auto range_extended(std::size_t count) {
-    return range_extended<T, value_type>(std::size_t {0}, count);
+constexpr auto range_extended(std::ptrdiff_t count) {
+    return range_extended<T, value_type>(std::ptrdiff_t {0}, count);
 }
 
 /**
  * @brief: Returned type of range_extended
  */
 template <typename T, typename value_type = typename T::value_type>
-    requires explicitly_convertible_to<std::size_t, value_type> &&
+    requires explicitly_convertible_to<std::ptrdiff_t, value_type> &&
                  std::is_constructible_v<T, value_type>
 using range_extended_t = decltype(range_extended<T, value_type>(0));
 
