13059286311cb1f5128fea5a57938e89932d92dc
Christian Brugger <brugger.chr@gmail.com>
2024-11-17 11:08:55 +0800

    Fixed linker selection on Windows

--- cmake/linker.cmake
+++ cmake/linker.cmake
@@ -8,20 +8,20 @@ macro(ls_setup_linker sanitizer_selection)
     # Most likely because it supports incremental builds.
     if (MSVC AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
         CMAKE_BUILD_TYPE STREQUAL "Debug" AND
-        sanitizer_selection STREQUAL "")
+        "${sanitizer_selection}" STREQUAL "")
         set(CMAKE_LINKER_TYPE MSVC)
     endif ()
 
     # Clang-CL Address Sanitizer is only supported with MSVC linker, not ldd-link
     if (MSVC AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND
-        sanitizer_selection STREQUAL "Address")
+        "${sanitizer_selection}" STREQUAL "Address")
         set(CMAKE_LINKER_TYPE MSVC)
     endif()
 
     # On Linux mold linker is faster, if available
     if (UNIX)
-        find_program(LS_LINK_PROGRAM mold)
-        if (LS_LINK_PROGRAM)
+        find_program(LS_LINK_PROGRAM_MOLD mold)
+        if (LS_LINK_PROGRAM_MOLD)
             set(CMAKE_LINKER_TYPE MOLD)
         endif()
     endif ()
