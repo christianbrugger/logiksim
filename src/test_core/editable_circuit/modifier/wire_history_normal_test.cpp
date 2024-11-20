a609312db56e50fe1db3145bbdd13f399a13997c
Christian Brugger <brugger.chr@gmail.com>
2024-11-20 14:26:03 +0700

    Simplified delete tests

--- src/test_core/editable_circuit/modifier/wire_history_normal_test.cpp
+++ src/test_core/editable_circuit/modifier/wire_history_normal_test.cpp
@@ -15,31 +15,7 @@ namespace editable_circuit {
 // Delete
 //
 
-TEST(EditableCircuitWireHistory, DeleteFullSegment) {
-    auto layout = Layout {};
-    const auto segment_index =
-        add_to_wire(layout, temporary_wire_id, SegmentPointType::shadow_point,
-                    ordered_line_t {point_t {0, 0}, point_t {10, 0}});
-    const auto segment = segment_t {temporary_wire_id, segment_index};
-    const auto segment_part = segment_part_t {segment, part_t {0, 10}};
-
-    auto modifier = get_modifier_with_history(layout);
-    {
-        auto segment_part_0 = segment_part;
-        modifier.delete_temporary_wire_segment(segment_part_0);
-    }
-    Expects(is_valid(modifier));
-
-    // before undo
-    ASSERT_EQ(get_segment_count(modifier.circuit_data().layout), 0);
-    ASSERT_EQ(are_normalized_equal(modifier.circuit_data().layout, layout), false);
-
-    // after undo
-    modifier.undo_group();
-    ASSERT_EQ(are_normalized_equal(modifier.circuit_data().layout, layout), true);
-}
-
-TEST(EditableCircuitWireHistory, DeleteSegmentKey) {
+TEST(EditableCircuitWireHistory, DeleteFullShadow) {
     auto layout = Layout {};
     const auto segment_index =
         add_to_wire(layout, temporary_wire_id, SegmentPointType::shadow_point,
@@ -64,6 +40,7 @@ TEST(EditableCircuitWireHistory, DeleteSegmentKey) {
     // after undo
     modifier.undo_group();
     ASSERT_EQ(segment_key, modifier.circuit_data().index.key_index().get(segment));
+    ASSERT_EQ(are_normalized_equal(modifier.circuit_data().layout, layout), true);
 }
 
 TEST(EditableCircuitWireHistory, DeleteFullCrosspoint) {
@@ -75,6 +52,7 @@ TEST(EditableCircuitWireHistory, DeleteFullCrosspoint) {
     const auto segment_part = segment_part_t {segment, part_t {0, 10}};
 
     auto modifier = get_modifier_with_history(layout);
+    const auto segment_key = modifier.circuit_data().index.key_index().get(segment);
     {
         auto segment_part_0 = segment_part;
         modifier.delete_temporary_wire_segment(segment_part_0);
@@ -87,10 +65,11 @@ TEST(EditableCircuitWireHistory, DeleteFullCrosspoint) {
 
     // after undo
     modifier.undo_group();
+    ASSERT_EQ(segment_key, modifier.circuit_data().index.key_index().get(segment));
     ASSERT_EQ(are_normalized_equal(modifier.circuit_data().layout, layout), true);
 }
 
-TEST(EditableCircuitWireHistory, DeletePartialSide) {
+TEST(EditableCircuitWireHistory, DeletePartialFront) {
     auto layout = Layout {};
     const auto segment_index =
         add_to_wire(layout, temporary_wire_id, SegmentPointType::shadow_point,
@@ -116,7 +95,33 @@ TEST(EditableCircuitWireHistory, DeletePartialSide) {
     ASSERT_EQ(segment_key, modifier.circuit_data().index.key_index().get(segment));
 }
 
-TEST(EditableCircuitWireHistory, DeletePartialMidle) {
+TEST(EditableCircuitWireHistory, DeletePartialEnd) {
+    auto layout = Layout {};
+    const auto segment_index =
+        add_to_wire(layout, temporary_wire_id, SegmentPointType::shadow_point,
+                    ordered_line_t {point_t {0, 0}, point_t {10, 0}});
+    const auto segment = segment_t {temporary_wire_id, segment_index};
+    const auto segment_part = segment_part_t {segment, part_t {5, 10}};
+
+    auto modifier = get_modifier_with_history(layout);
+    const auto segment_key = modifier.circuit_data().index.key_index().get(segment);
+    {
+        auto segment_part_0 = segment_part;
+        modifier.delete_temporary_wire_segment(segment_part_0);
+    }
+    Expects(is_valid(modifier));
+
+    // before undo
+    ASSERT_EQ(get_segment_count(modifier.circuit_data().layout), 1);
+    ASSERT_EQ(are_normalized_equal(modifier.circuit_data().layout, layout), false);
+
+    // after undo
+    modifier.undo_group();
+    ASSERT_EQ(are_normalized_equal(modifier.circuit_data().layout, layout), true);
+    ASSERT_EQ(segment_key, modifier.circuit_data().index.key_index().get(segment));
+}
+
+TEST(EditableCircuitWireHistory, DeletePartialMiddle) {
     auto layout = Layout {};
     const auto segment_index =
         add_to_wire(layout, temporary_wire_id, SegmentPointType::shadow_point,
@@ -230,10 +235,8 @@ TEST(EditableCircuitWireHistory, AddTemporary) {
     auto layout = Layout {};
 
     auto modifier = get_modifier_with_history(layout);
-    {
-        modifier.add_wire_segment(ordered_line_t {point_t {0, 0}, point_t {10, 0}},
-                                  InsertionMode::temporary);
-    }
+    modifier.add_wire_segment(ordered_line_t {point_t {0, 0}, point_t {10, 0}},
+                              InsertionMode::temporary);
     Expects(is_valid(modifier));
 
     // before undo
