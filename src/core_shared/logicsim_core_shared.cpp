9b3c83b1eb7de49f625a05c6e7915835dd399f08
Christian Brugger <brugger.chr@gmail.com>
2025-01-31 15:31:51 +0800

    Hour-class c/c++ interface for dll

--- src/core_shared/logicsim_core_shared.cpp
+++ src/core_shared/logicsim_core_shared.cpp
@@ -1,9 +1,70 @@
 #include "core_shared/logicsim_core_shared.h"
 
+#include "core/circuit_example.h"
+#include "core/editable_circuit.h"
+#include "core/vocabulary/simulation_config.h"
+#include "core/vocabulary/view_config.h"
+
+#include <gsl/gsl>
+
 namespace logicsim {
 
-auto test() -> int {
-    return 13;
+//
+// Exported Circuit Impl
+//
+
+class ExportedCircuit_Impl {
+   public:
+    auto load_circuit(int number) -> void;
+
+   private:
+    ViewConfig view_config_ {};
+    SimulationConfig simulation_config_ {};
+    EditableCircuit editable_circuit_ {Layout {},
+                                       EditableCircuit::Config {.enable_history = true}};
+};
+
+auto ExportedCircuit_Impl::load_circuit(int number) -> void {
+    editable_circuit_ = load_example_with_logging(number);
 }
 
 }  // namespace logicsim
+
+//
+// C DLL Interface
+//
+
+// extern "C" {
+struct ls_circuit {
+    logicsim::ExportedCircuit_Impl data;
+};
+
+// }
+
+template <typename Func>
+auto ls_translate_exception(Func&& func) {
+    try {
+        return std::invoke(func);
+    } catch (...) {
+        std::terminate();
+    }
+}
+
+auto ls_circuit_construct() -> ls_circuit_t {
+    return ls_translate_exception([]() { return new ls_circuit; });
+}
+
+auto ls_circuit_destruct(ls_circuit_t obj) -> void {
+    ls_translate_exception([&]() { delete obj; });
+}
+
+auto ls_circuit_load(ls_circuit_t obj, int32_t example_circuit) -> void {
+    ls_translate_exception([&]() {
+        Expects(obj);
+        obj->data.load_circuit(example_circuit);
+    });
+}
+
+auto ls_test() -> int {
+    return ls_translate_exception([]() { return 13; });
+}
--- src/core_shared/logicsim_core_shared.h
+++ src/core_shared/logicsim_core_shared.h
@@ -1,6 +1,12 @@
 #ifndef LOGICSIM_CORE_LOGICSIM_SHARED_H
 #define LOGICSIM_CORE_LOGICSIM_SHARED_H
 
+#ifdef __cplusplus
+#include <cstdint>
+#else
+#include <stdint.h>
+#endif
+
 #ifdef _WIN32
 #ifdef LS_CORE_LIB_BUILD_SHARED
 #define LS_CORE_API __declspec(dllexport)
@@ -11,10 +17,87 @@
 #define LS_CORE_API
 #endif
 
-namespace logicsim {
+//
+// C DLL interface - hourclass pattern
+//
 
-[[nodiscard]] LS_CORE_API auto test() -> int;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+typedef enum {
+    ls_example_circuit_1 = 1,
+    ls_example_circuit_2 = 2,
+    ls_example_circuit_3 = 3,
+    ls_example_circuit_4 = 4,
+} ls_example_circuit_t;
+
+typedef struct ls_circuit* ls_circuit_t;
+
+ls_circuit_t LS_CORE_API ls_circuit_construct();
+void LS_CORE_API ls_circuit_destruct(ls_circuit_t obj);
+void LS_CORE_API ls_circuit_load(ls_circuit_t obj, int32_t example_circuit);
+
+int LS_CORE_API ls_test();
+
+#ifdef __cplusplus
 }
+#endif
+
+//
+// C++ abstraction - header only
+//
+
+#ifdef __cplusplus
+
+#include <memory>  // unique_ptr
+
+namespace logicsim {
+namespace core {
+
+enum class ExampleCircuitType : uint8_t {
+    example_circuit_1 = 1,
+    example_circuit_2 = 2,
+    example_circuit_3 = 3,
+    example_circuit_4 = 4,
+};
+
+namespace detail {
+
+struct LSCircuitDeleter {
+    auto operator()(ls_circuit_t obj) -> void {
+        ls_circuit_destruct(obj);
+    };
+};
+
+}  // namespace detail
+
+class CircuitInterface {
+   public:
+    auto load(ExampleCircuitType type) -> void;
+
+   private:
+    std::unique_ptr<ls_circuit, detail::LSCircuitDeleter> obj_ {ls_circuit_construct()};
+};
+
+//
+// C++ abstraction - Implementation
+//
+
+auto ls_expects(auto&& value) -> void {
+    if (!bool {value}) {
+        std::terminate();
+    }
+}
+
+auto CircuitInterface::load(ExampleCircuitType type) -> void {
+    ls_expects(obj_);
+    ls_circuit_load(obj_.get(), static_cast<int32_t>(type));
+};
+
+}  // namespace core
+}  // namespace logicsim
+
+#endif
 
 #endif
