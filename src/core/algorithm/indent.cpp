5b5900297bb0b6e27a3b6008856b619a0d7dc4c7
Christian Brugger <brugger.chr@gmail.com>
2025-01-31 11:53:09 +0800

    Moved indent function to cpp

--- src/core/algorithm/indent.cpp
+++ src/core/algorithm/indent.cpp
@@ -1,7 +1,31 @@
 #include "core/algorithm/indent.h"
 
+#include "core/iterator_adaptor/transform_view.h"
+
+#include <boost/algorithm/string/join.hpp>
+#include <fmt/core.h>
+#include <gsl/gsl>
+
+#include <ranges>
+
 namespace logicsim {
 
-//
+auto indent(std::string_view input, int count) -> std::string {
+    Expects(count >= 0);
+    if (count == 0) {
+        return std::string {input.begin(), input.end()};
+    }
 
+    const auto add_indent = [count](auto &&line) -> std::string {
+        return fmt::format("{:{}}{}", "", count,
+                           std::string_view {line.begin(), line.end()});
+    };
+
+    auto split = std::views::split(input, '\n');
+
+    // TODO refactor in c++23 to use std::views::join_with and std::views::transform
+    // use our own transform view, as boost does not work with std::views::transform
+    return boost::join(transform_view(split, add_indent), "\n");
 }
+
+}  // namespace logicsim
